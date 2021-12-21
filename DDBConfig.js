6588093efc642ad0472ceb411258533be073eba4
PrernaCompro <prerna.jain@comprotechnologies.com>
2021-12-21 16:54:03 +0530

    initial commit

--- DDBConfig.js
+++ DDBConfig.js
@@ -1,8 +1,8 @@
 var aws = require('aws-sdk')
 
 var ddb = new aws.DynamoDB.DocumentClient({
-        accessKeyId: 'AKIAYIHNXSALZ7JQGRHM',//process.env.accessKey,
-      secretAccessKey: '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',//process.env.secretAccessKey,
+        accessKeyId: process.env.accessKey,
+      secretAccessKey: process.env.secretAccessKey,
       region: 'us-east-1'
 })
 
